530ca57a38bec5ab8cef02220e3f730ff7c255c6
pmbrull <peremiquelbrull@gmail.com>
2019-08-16 18:13:33 +0200

    Add local README + spline jar

--- local/scripts/generate.sh
+++ local/scripts/generate.sh
@@ -10,7 +10,7 @@ kubectl create secret generic shared-bootstrap-data --from-file=internal-auth-mo
 rm $TMPFILE
 
 # Create mongodb service with mongod stateful-set
-# TODO: Temporarily added no-valudate due to k8s 1.8 bug: https://github.com/kubernetes/kubernetes/issues/53309
+# TODO: Temporarily added no-validate due to k8s 1.8 bug: https://github.com/kubernetes/kubernetes/issues/53309
 kubectl apply -f ../resources/mongodb-service.yaml --validate=false
 sleep 5
 
